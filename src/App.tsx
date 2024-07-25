809b1be0d6fb5eb44b389e0eb0ecd55365be320d
jangmyeongji <2hundred2ne2@gmail.com>
2024-07-25 16:14:20 +0900

    리듀서

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import Todo from "./components/Todo";
-import TodoList from "./components/TodoList";
-
+import { useReducer, useState } from "react";
+const reducer = (
+  state: number,
+  action: { type: string; payload?: number | undefined }
+) => {
+  if (action.type === "increment" && action.payload) {
+    return state + action.payload;
+  } else if (action.type === "decrement" && action.payload) {
+    return state - action.payload;
+  } else if (action.type === "reset") {
+    return 0;
+  } else {
+    return state;
+  }
+};
 const App = () => {
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
     <>
-      <Todo />
+      <h1>count : {count}</h1>
+      <button onClick={() => dispatch({ type: "decrement", payload: 4 })}>
+        감소
+      </button>
+      <button onClick={() => dispatch({ type: "reset", payload: 5 })}>0</button>
+      <button onClick={() => dispatch({ type: "increment", payload: 5 })}>
+        증가
+      </button>
     </>
   );
 };
